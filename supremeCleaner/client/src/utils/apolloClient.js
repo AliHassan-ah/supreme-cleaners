bb6ef1d4847848e3c93b24793afaa194531cb697
Ali <alihassan.ah3535@gmail.com>
2023-11-14 22:58:32 +0500

    Started Admin Pannel

--- supremeCleaner/client/src/utils/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_ADMIN_BASE_URL}/graphql`,
-  cache: new InMemoryCache(),
-});
-
-export default client;
